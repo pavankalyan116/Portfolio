88de51649d7d90ff16bf2cf1665583de0d91f7d3
Mekalasreepranavi <kspkalyan116@gmail.com>
2025-06-02 09:49:09 +0530

    index update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Set root to project directory
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Path to your index.html
+    },
+  },
 })
